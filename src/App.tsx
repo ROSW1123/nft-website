9beb2be346162481c386c164bf6a01f027615880
ROSW1123 <153902036+ROSW1123@users.noreply.github.com>
2024-01-19 19:27:07 +0800

    new

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+// import { ConnectWallet } from "@thirdweb-dev/react";
+import { ThirdwebNftMedia, useContract, useContractMetadata, useNFTs } from "@thirdweb-dev/react";
+import "./styles/Home.css";
+
+export default function Home() {
+  const { contract } = useContract('0x09b52851735d76615BcA080e8c7ed64F561D54c3');
+  const { data: nfts, isLoading } = useNFTs(contract);
+  const { data: metadata, isLoading: loadingMetadata } = useContractMetadata(contract);
+  return (
+    <main className="container">
+      {!loadingMetadata &&
+        <header className="heading">
+          <div>
+            <img src={metadata?.image} alt="NFT Collection Thumbnail" />
+            <h1>{metadata?.name}</h1>
+          </div>
+        </header>
+      }
+
+      {!isLoading ?
+      (<div className="gallery">
+        {nfts?.map(e =>
+          <div className="card">
+            <ThirdwebNftMedia metadata={e.metadata} />
+          </div>
+        )}
+      </div>)
+      : (<p className="loading">Loading...</p>) }
+    </main>
+  );
+}
