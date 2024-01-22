1fb2e421cc6359bb6c012505a3ac12deedbafd0a
ROSW1123 <153902036+ROSW1123@users.noreply.github.com>
2024-01-22 19:09:27 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,7 @@ export default function Home() {
   const { contract } = useContract('0x09b52851735d76615BcA080e8c7ed64F561D54c3');
   const { data: nfts, isLoading } = useNFTs(contract);
   const { data: metadata, isLoading: loadingMetadata } = useContractMetadata(contract);
-  const { data: primarySaleRecipient, error } = usePrimarySaleRecipient(contract);
-  const { data: ownedNFTs } = useOwnedNFTs(contract, "0xFEc795EC001d1B0E76aaF85589dA52Ce0a4daafe", { start: 0, count: 100 });
-  console.log(ownedNFTs);
-  console.log(metadata)
+  console.log(nfts)
   return (
     <main className="container">
       {!loadingMetadata &&
@@ -21,12 +18,6 @@ export default function Home() {
           </div>
         </header>
       }
-
-      <div>{primarySaleRecipient}</div>
-      <div>{ownedNFTs?.map(ee=>
-        <div>
-          <p>{ownedNFTs[0].owner}</p>
-        </div>)}</div>
 
       {!isLoading ?
       (<div className="gallery">
